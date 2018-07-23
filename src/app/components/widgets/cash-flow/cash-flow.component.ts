d878842b8fb9b2678b394582bc384fe01461b344
Eimantas <eimyss@gmail.com>
2018-07-23 20:34:08 +0200

    i really hate css

--- src/app/components/widgets/cash-flow/cash-flow.component.ts
+++ src/app/components/widgets/cash-flow/cash-flow.component.ts
@@ -3,9 +3,67 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cash-flow',
   templateUrl: './cash-flow.component.html',
-  styleUrls: ['./cash-flow.component.css']
+  styleUrls: ['./cash-flow.component.scss']
 })
 export class CashFlowComponent implements OnInit {
+
+
+  public lineChartData:Array<any> = [
+      {data: [14000, 15000, 14880, 8100, 20000, 18000, 20000], label: 'Business Konto'},
+      {data: [2800, 3900, 6000, 7900, 9000, 8000, 9998], label: 'Privat Konto'}
+    ];
+    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions:any = {
+      responsive: true
+    };
+    public lineChartColors:Array<any> = [
+      { // grey
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      },
+      { // dark grey
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)'
+      },
+      { // grey
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      }
+    ];
+    public lineChartLegend:boolean = true;
+    public lineChartType:string = 'line';
+
+    public randomize():void {
+      let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+      for (let i = 0; i < this.lineChartData.length; i++) {
+        _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+          _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+        }
+      }
+      this.lineChartData = _lineChartData;
+    }
+
+    // events
+    public chartClicked(e:any):void {
+      console.log(e);
+    }
+
+    public chartHovered(e:any):void {
+      console.log(e);
+    }
 
   constructor() { }
 
--- src/app/expenses-overview/expenses-overview.component.ts
+++ src/app/expenses-overview/expenses-overview.component.ts
@@ -9,7 +9,7 @@ import { AccountDTO } from '../shared/dto/accountDTO';
 @Component({
   selector: 'app-expenses-overview',
   templateUrl: './expenses-overview.component.html',
-  styleUrls: ['./expenses-overview.component.css']
+  styleUrls: ['./expenses-overview.component.scss']
 })
 export class ExpensesOverviewComponent implements OnInit {
 
@@ -20,13 +20,16 @@ public barChartOptions:any = {
   scaleShowVerticalLines: false,
   responsive: true
 };
-public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+public barChartLabels:string[] = ['KW10', 'KW11', 'KW12', 'KW13', 'KW14', 'KW15', 'KW16'];
 public barChartType:string = 'bar';
 public barChartLegend:boolean = true;
 
 public barChartData:any[] = [
-  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  {data: [10, 8, 10, 0, 10, 40, 10], label: 'Essen'},
+  {data: [20, 15, 15, 15, 35, 15, 15], label: 'Fahrkosten'},
+  {data: [0, 200, 0, 0, 50, 15, 0], label: 'Sonstige Ausgaben'},
+    {data: [0, 0, 0, 100, 0, 0, 90], label: 'Private Ausgaben'}
+
 ];
 
 // events
