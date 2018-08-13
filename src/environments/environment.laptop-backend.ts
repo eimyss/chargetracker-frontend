95216903536208daa9a00af0d6a4841e8c8d5397
Eimantas <eimyss@gmail.com>
2018-08-13 16:52:57 +0200

    too mucch envs

--- src/environments/environment.laptop-backend.ts
+++ src/environments/environment.laptop-backend.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
+    title: 'eimantas expenses (eimantas-pc-backend)',
     clienturl: 'localhost:4200',
-    API: 'http://10.8.0.2:8081',
+    API: 'http://10.8.0.4:8081',
       backend: true
 };
