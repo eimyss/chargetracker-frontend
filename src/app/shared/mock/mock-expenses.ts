88dd15d684207f840d8610a8cc3f5f567379b37f
Eimantas <eimyss@gmail.com>
2018-09-03 20:00:06 +0200

    mocked expenses

--- src/app/shared/mock/mock-expenses.ts
+++ src/app/shared/mock/mock-expenses.ts
@@ -1,20 +1,8 @@
 import { Expense } from '../dto/expense';
 
 export const expenses: Expense[] = [
-  { id: 11, name: 'Bier', betrag: 11.4, ort: "Bingen", href: "", expensable: true, type: "Food", account: "Konto-1", expenseDate: "01/02/2018", createDate: "01/02/2018",giphyUrl: "" },
-  { id: 11, name: 'Essen', betrag: 11.4,ort: "FFM"  , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
-  { id: 11, name: 'diesel', betrag: 11.4, ort: "Bonn"  , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
-
-  { id: 11, name: 'Bier', betrag: 11.4, ort: "Bingen", href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
-  { id: 11, name: 'Essen', betrag: 11.4,ort: "FFM"  , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018" ,giphyUrl: ""},
-  { id: 11, name: 'diesel', betrag: 11.4, ort: "Bonn" , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018" ,giphyUrl: ""},
-
-  { id: 11, name: 'Bier', betrag: 11.4, ort: "Bingen", href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
-  { id: 11, name: 'Essen', betrag: 11.4,ort: "FFM" , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018" ,giphyUrl: ""},
-  { id: 11, name: 'diesel', betrag: 11.4, ort: "Bonn" , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018" ,giphyUrl: ""},
-
-  { id: 11, name: 'Bier', betrag: 11.4, ort: "Bingen" , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
-  { id: 11, name: 'Essen', betrag: 11.4,ort: "FFM" , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018" ,giphyUrl: ""},
-  { id: 11, name: 'diesel', betrag: 11.4, ort: "Bonn"  , href: "", expensable: true, type: "Food", account: "Konto-1", createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" }
+  { id: 10, name: 'Bier', betrag: 11.4, ort: "Bingen", href: "", expensable: true, type: "Food", account: "Konto-1",accountid: 10, expenseDate: "01/02/2018", createDate: "01/02/2018",giphyUrl: "" },
+  { id: 11, name: 'Essen', betrag: 11.4,ort: "FFM"  , href: "", expensable: true, type: "Food", account: "Konto-1",accountid: 10, createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" },
+  { id: 12, name: 'diesel', betrag: 11.4, ort: "Bonn"  , href: "", expensable: true, type: "Food", account: "Konto-1",accountid: 11, createDate: "01/02/2018", expenseDate: "01/02/2018",giphyUrl: "" }
 
 ];
