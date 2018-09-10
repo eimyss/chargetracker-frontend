43a80d8b2a907f0975c6b7eb42d608821d74d804
Eimantas <eimyss@gmail.com>
2018-09-10 21:26:04 +0200

    dashboard

--- src/app/expenses-navigation/expenses-navigation.component.ts
+++ src/app/expenses-navigation/expenses-navigation.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { KeycloakService } from '../shared/service/keycloack.service';
 import { EnvironmentService } from '../shared/environment/environment.service';
+import { AccountCacheService } from '../shared/service/cache/account-cache.service';
+import { AccountDTO } from '../shared/dto/accountDTO';
 
 @Component({
   selector: 'expenses-navigation',
@@ -14,19 +16,19 @@ export class ExpensesNavigationComponent {
 
   title = 'app';
   isAuthenticated: boolean;
-
+  accountList: AccountDTO[];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private keycloack: KeycloakService, private env: EnvironmentService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private keycloack: KeycloakService, private env: EnvironmentService, private accountcache: AccountCacheService) {}
 
   async ngOnInit() {
 
     this.title = this.env.title;
-
+    this.accountList = this.accountcache.getAccountList();
     console.log('keylocak is loggedin: ' + this.keycloack.isLoggedIn());
     this.isAuthenticated = await this.keycloack.isLoggedIn();
   }
