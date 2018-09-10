8e474f67d8e042183333d6822b378800773a3a57
Eimantas <eimyss@gmail.com>
2018-09-10 20:07:23 +0200

    dashboard first shapes

--- src/app/expenses-dashboard/expenses-dashboard.component.ts
+++ src/app/expenses-dashboard/expenses-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DashboardPageComponent } from '../pages/dashboard-page/dashboard-page.component';
 
 @Component({
   selector: 'ExpensesdashboardComponent',
@@ -7,9 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ExpensesDashboardComponent {
   cards = [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+    { title: 'Card 1', cols: 2, rows: 1, component: DashboardPageComponent },
+    { title: 'Card 2', cols: 1, rows: 1, component: DashboardPageComponent  },
+    { title: 'Card 3', cols: 1, rows: 2 , component: DashboardPageComponent },
+    { title: 'Card 4', cols: 1, rows: 1 , component: DashboardPageComponent }
   ];
 }
