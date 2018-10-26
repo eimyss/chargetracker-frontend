288afe42927f24d811b2a5f685f1fd7271181596
eimyss <eimyss@gmail.com>
2018-10-26 17:54:05 +0200

    added another realm and ref

--- src/environments/environment.ref.ts
+++ src/environments/environment.ref.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clienturl: 'https://ref.eimantas-services.de',
   authUrl: 'https://security.eimantas-services.de',
-  title: 'Eimantas - Expense App v.01 (REF)',
-  API: 'https://backend.eimantas-services.de',
+  title: 'Eimantas - Expense App v.02 (REF)',
+  API: 'https://backend-ref.eimantas-services.de',
   backend: true
 };
