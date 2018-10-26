52d946f32f0d8e60471b68038912e1b0b59b42b4
eimyss <eimyss@gmail.com>
2018-10-26 15:57:59 +0200

    added ref in frontend

---
+++ src/environments/environment.ref.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  clienturl: 'https://ref.eimantas-services.de',
+  authUrl: 'https://security.eimantas-services.de',
+  title: 'Eimantas - Expense App v.01 (REF)',
+  API: 'https://backend.eimantas-services.de',
+  backend: true
+};
