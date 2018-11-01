dbf58b4b5dd35c2cafade0cca85af12ca05d8d27
eimyss <eimyss@gmail.com>
2018-11-01 20:15:01 +0100

    finished first project info

--- src/environments/environment.ref.ts
+++ src/environments/environment.ref.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clienturl: 'https://ref.eimantas-services.de',
   authUrl: 'https://security.eimantas-services.de',
-  title: 'Eimantas - Expense App v.02 (REF)',
+  title: 'Eimantas - Expense App v.03 (REF - Project Edition)',
   API: 'https://backend-ref.eimantas-services.de',
   backend: true
 };
